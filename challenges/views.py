7ce7d70f1167b44ecbd927fae3eb7702383b12ee
tayson13 <ab.naser01@gmail.com>
2023-08-01 02:27:56 +0600

    16-10 done

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    # return HttpResponse('Hello World')
+
+    meetups = [
+        {'title': 'First Meetup', 'location': 'Banani', 'slug': 'first'},
+        {'title': 'Second Meetup', 'location': 'Istanbul', 'slug': 'second'},
+    ]
+
+    return render(request, 'challenges/index.html', {
+        'showMeetup': True,
+        'meetups': meetups
+    })
+
+
+def meetup_details(request):
+    selected_meetup = {'title': 'First Meetup', 'location': 'Banani', 'slug': 'first', 'description': 'sdf khjsd;fj dsjfkl jdsa;f ' },
+
+    return render(request, 'challenges/meetup_details.html', {
+        'selected_meetup': selected_meetup
+    })
